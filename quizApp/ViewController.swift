d1bc6f4ac80a25a46f13d6c9e5e743a702f28dfe
Alyssa <ataltimas@gmail.com>
2020-07-24 09:19:35 -0500

    Initial Commit

---
+++ quizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quizApp
+//
+//  Created by Alyssa Altimas on 7/24/20.
+//  Copyright © 2020 Alyssa Altimas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
